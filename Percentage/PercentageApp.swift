fe30659c99112e4b227e9204fdc0a9d3eca8b62d
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-13 22:44:28 +0100

    Initial Commit

---
+++ Percentage/PercentageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PercentageApp.swift
+//  Percentage
+//
+//  Created by  Ixart on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PercentageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

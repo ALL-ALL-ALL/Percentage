13892a0de1dda01c2521cc6a88ec31f3aa161d36
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-14 20:40:03 +0100

    j'ai refait avec un rectangle plus approprier a ce qui est demandé

--- Percentage/ContentView.swift
+++ Percentage/ContentView.swift
@@ -8,30 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var progress = 0.0
     
     var body: some View {
         ZStack {
-            Image(systemName: "battery.0")
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(.red)
-                .background(Rectangle()
-                    .fill(Color.green)
-                    .scaleEffect(CGSize(width: 1.0, height: 1.0), anchor: .leading))
+            Rectangle()
+                .foregroundColor(.purple)
+                .cornerRadius(10)
+                .frame(width: 290, height: 60)
+            
+            Text("100%")
+                .font(.largeTitle)
+                .bold()
             
             
-                .mask(
-                    Image(systemName: "battery.100")
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundColor(.red)
-                
-                ) // FIN DE MASK
-            Text("\(Int(self.progress * 100))%")
             
-              
-        } // FIN VSTACK
+        } // FIN ZSTACK
         
         
         .padding()

de0f76643b70d73d0d0af57049658ca84d9e6d43
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-14 18:54:47 +0100

    il manque la progression du vert pour croire a une batterie charger

--- Percentage/ContentView.swift
+++ Percentage/ContentView.swift
@@ -8,16 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        ZStack {
+            Image(systemName: "battery.0")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.red)
+                .background(Rectangle()
+                    .fill(Color.green)
+                    .scaleEffect(CGSize(width: 1.0, height: 1.0), anchor: .leading))
+            
+            
+                .mask(
+                    Image(systemName: "battery.100")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.red)
+                
+                ) // FIN DE MASK
+            Text("\(Int(self.progress * 100))%")
+            
+              
+        } // FIN VSTACK
+        
+        
         .padding()
-    }
-}
+        
+    } // FIN BODY
+} // FIN STRUCT
 
 #Preview {
     ContentView()

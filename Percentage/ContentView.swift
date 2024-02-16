d1972d68c9b47898466f7d125e31d24cdce88ed0
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-16 12:52:46 +0100

    intergration de vortex pour la 1er fois package

--- Percentage/ContentView.swift
+++ Percentage/ContentView.swift
@@ -6,21 +6,32 @@
 //
 
 import SwiftUI
+import Vortex
 
 struct ContentView: View {
     @State private var progress = 0.0
     
     var body: some View {
+        
+        
         ZStack  {
+            VortexView(.rain) {
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 32)
+                    .tag("circle")
+            }
             Rectangle()
                 .cornerRadius(19)
                 .frame( width: 290, height: 60)
                 .foregroundColor(.purple)
                 .scaleEffect(x: progress, y: 1, anchor: .leading)
                 .animation(.spring(response: 0.5, dampingFraction: 2.7))
+            
                 .onAppear {
                                     progress = 1.0
-                                }
+                    
+                                } // on appear 
 
 
                
